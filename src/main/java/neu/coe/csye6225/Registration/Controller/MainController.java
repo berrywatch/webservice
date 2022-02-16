1be5af7eea3f13b5fbfbb4313ba5e786a298aee4
Kan Zhang <zhangkan1111@gmail.com>
2022-02-15 23:48:56 -0500

    Add basic auth. Meet all API requirements

--- src/main/java/neu/coe/csye6225/Registration/Controller/MainController.java
+++ src/main/java/neu/coe/csye6225/Registration/Controller/MainController.java
@@ -1,18 +1,11 @@
 package neu.coe.csye6225.Registration.Controller;
 
 import neu.coe.csye6225.Registration.Entity.User;
-import neu.coe.csye6225.Registration.Entity.UserUpdate;
-import neu.coe.csye6225.Registration.Repository.UserRepository;
 import neu.coe.csye6225.Registration.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(path="/v1/user")
@@ -32,23 +25,17 @@ public class MainController {
     }
 
     @PutMapping(path="/self")
-    public ResponseEntity<String> update(@RequestBody UserUpdate uu){
-
-        if(!userService.update(uu)){
+    public ResponseEntity<String> update(@RequestBody User uu, @RequestHeader("authorization") String authorization){
+        if(!userService.update(uu,authorization)){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>("Updated", HttpStatus.NO_CONTENT);
     }
 
-//    @GetMapping(path="/self")
-//    public ResponseEntity<User> get(@RequestParam(defaultValue = "") String email, @RequestHeader("Authorization") String token){
-//        if(!token.equals(this.token)){
-//            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-//        }
-//        if(!userRepository.findById(email).isPresent()){
-//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-//        }
-//        Optional<User> u = userRepository.findById(email);
-//        return new ResponseEntity<User>(u.get(),HttpStatus.OK);
-//    }
+    @GetMapping(path="/self")
+    public ResponseEntity<User> get(@RequestHeader("authorization") String authorization){
+        User u = userService.get(authorization);
+        if(u==null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<User>(u,HttpStatus.OK);
+    }
 }
